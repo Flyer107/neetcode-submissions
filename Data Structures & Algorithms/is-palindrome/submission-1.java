fc8c1dc0507260aaf6eed5c99c2a1a68c6947a08
Flyer107 <43021952+Flyer107@users.noreply.github.com>
2026-09-03 21:14:42 -0400

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder newStr = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                newStr.append(Character.toLowerCase(c));
+            }
+        }
+        return newStr.toString().equals(newStr.reverse().toString());
+    }
+}
+
+/*
+This is also a neat solution, but it uses more space.
+
+Time: O(n)
+Space: O(n) because of the string builder
+
+*/
