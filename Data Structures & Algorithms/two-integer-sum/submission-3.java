a470d02e554531a4e9140fb5f39d36caa787b46e
Flyer107 <43021952+Flyer107@users.noreply.github.com>
2026-09-24 09:49:38 -0400

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.java
@@ -0,0 +1,21 @@
+class Solution {
+    // Brute Force
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i != j && nums[i] + nums[j] == target) {
+                    res[0] = i;
+                    res[1] = j;
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+}
+
+/*
+Time: O(n^2) b/c of the double for loop
+Space: O(1) because constant allocation for the result*/
